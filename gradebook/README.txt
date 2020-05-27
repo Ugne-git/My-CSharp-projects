233a3abc470538b29816ae0deba19e8415eeea3a
Ugne-git <61318843+Ugne-git@users.noreply.github.com>
2020-05-27 11:34:15 +0300

    Adding Lists and calculations

---
+++ gradebook/README.txt
@@ -0,0 +1,3 @@
+The purpose of this project is to create an electronic grade book to read the scores of an individual student and then compute some simple statistics from the scores.
+
+The grades are entered as floating point numbers from 0 to 10, and the statistics show us the highest grade, the lowest grade, and the average grade.
--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,7 +7,17 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            
+            var grades = new List<double>() {7.7, 9.3, 8, 5};
+            grades.Add(7.7);
+
+            var result = 0.0;
+            foreach(var number in grades)
+            {
+                result += number;
+            }
+            result /= grades.Count;
+            System.Console.WriteLine($"The average grade is {result:N2}");
+
             if (args.Length > 0)
             {
             Console.WriteLine($"Hello, {args[0]}!");
