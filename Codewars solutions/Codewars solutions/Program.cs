6a804ed9d420c71944b6aa8ba192706be36fc174
Ugne-git <61318843+Ugne-git@users.noreply.github.com>
2020-06-04 12:06:59 +0300

    adding SumMultiples()
    method takes a given value and sums all natural numbers below that value that are multiples of 3 and of 5

--- Codewars solutions/Codewars solutions/Program.cs
+++ Codewars solutions/Codewars solutions/Program.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Codewars_solutions
 {
-    class Number
+    class Solutions
     {
         static void Main(string[] args)
         {
             //TestHowModulusWorks();
-            DigitalRoot();
-            Console.ReadLine();
+            //DigitalRoot();
+           
+            SumMultiples();
+            //Console.ReadLine();
         }
         //Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing 
         //in this way until a single-digit number is produced. This is only applicable to the natural numbers.
@@ -49,27 +53,66 @@ namespace Codewars_solutions
 
         static void TestHowModulusWorks()
         {
-            //int result;
-            //int number;
-
             while (true)
-            { 
-            Console.WriteLine("Enter a number or type 'q' to quit");
-            var input = Console.ReadLine();
-            
+            {
+                Console.WriteLine("Enter a number or type 'q' to quit");
+                var input = Console.ReadLine();
+
                 if (input == "q")
                 {
                     break;
                 }
                 else
                 {
-                   var number = int.Parse(input);
+                    var number = int.Parse(input);
                     int result = number % 10;
                     Console.WriteLine(result);
                 }
-                
             }
-            
+        }
+        //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+        //The sum of these multiples is 23.
+        //Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+        //Note: If the number is a multiple of both 3 and 5, only count it once.
+
+        static void SumMultiples()
+
+        {
+            int sum = 0;
+            Console.WriteLine("Enter a number: ");
+            var input = Console.ReadLine();
+            int value = int.Parse(input);
+            while (value > 0)
+            {
+                value = value - 1;
+                if (value % 3 == 0 || value % 5 == 0)
+                {
+                    sum = value + sum;
+                }
+            }
+
+            Console.WriteLine($"The sum of multiples of 3 and 5 is: {sum}");
+            Console.WriteLine("***********");
+            UserInput();
+        }
+        static void UserInput()
+        {
+        Console.WriteLine("Press 1 - to continue, or 2 - to quit");
+            var input = Console.ReadLine();
+      
+            switch (input)
+            {
+                case "1":
+                    Console.Clear();
+                    SumMultiples();
+                    break;
+                case "2":
+                    Console.Clear();
+                    Console.WriteLine("Good Bye!");
+                    Thread.Sleep(1500);
+                    break;
+            }
+
         }
     }
 }
